cd2ff3dfb4cc51327cb9df83c34a100ab70434d7
Effy Wang <effywn@Effys-MacBook-Pro.local>
2024-03-26 13:25:50 -0400

    add data and gateway

--- Filelog-KV/data.hpp
+++ Filelog-KV/data.hpp
@@ -3,12 +3,20 @@
 #include <array>
 #include <cstdint>
 #include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include "tcp_utils.hpp"
+#include "udp_utils.hpp"
+#include <iostream>
+
+#define BLOCK_SIZE 4096
 
 using key_t = int32_t;
-using value_t = int32_t;
 using nblk_t = int16_t;
-using version_t = int32_t;
+// using version_t = int32_t;
 
+/* at gateways
+ */
 
 // cmd.op types
 enum CmdType
@@ -16,23 +24,24 @@ enum CmdType
 	READ = 1,
 	APPEND = 2,
 };
-
 // gateways translate KV request to cmd
 struct cmd {
 	CmdType op;
-	nblk_t nblks;
-	off_t lba;
+	key_t key;
+	char value[BLOCK_SIZE] = {0};	
+};
+// gateways keep a K-LBAs map in a map manner
+// K_LBAs = std::map<key_t,LBAs>, not sure.
+// in fact, the MapVal can also serve as a vector clock because each LBAs only grows up.
+struct LBAs {
+	std::array<off_t, 3> lbas;
 };
 
-// gateways keep a K-LBAs map in a map manner
-// K_LBAs = std::map<key_t, MapVal>, not sure.
-// in fact, the MapVal can also serve as a vector clock because each LBAs only grows up.
-struct MapVal {
-	std::array<off_t, 3> LBAs;
+struct LoggerReply {
+	int16_t _logger_id;
+    key_t key;
+    off_t lba;
 };
-// {
-// 	std::tuple<off_t, off_t, off_t> LBAs;
-// };
 
 /*
 class LoggedMap
@@ -56,7 +65,6 @@ public:
 	void apply_up_to(oplogidx_t idx);
 };
 */
-
 enum RequestType
 {
 	GET = 1,
@@ -68,14 +76,13 @@ struct KVRequest
 {
 	RequestType request_type;
 	key_t key;
-	value_t value = 0;
+	char value[BLOCK_SIZE] = {0};
 };
-
 
 struct ReplicationRequest 
 {
 	key_t key;
-	MapVal LBAs;
+	LBAs LBAs;
 };
 
 struct ReplicationResp 
@@ -104,4 +111,38 @@ enum ConnType
 struct ConnHeader 
 {
 	ConnType type;
+};
+
+
+/*
+	at loggers
+ */
+struct header_t {
+	key_t key;
+	ssize_t length;
+	// char padding[BLOCK_SIZE-sizeof(key_t)-sizeof(ssize_t)] = {0};
+};
+
+struct LogEnt {
+    header_t metadata;
+    char data[BLOCK_SIZE-sizeof(header_t)] = {0};
+
+	void set_key(key_t key) {
+		metadata.key = key;
+	}
+	void set_length(ssize_t length) {
+		metadata.length = length;
+	}
+	key_t get_key() {
+		return metadata.key;
+	}
+	ssize_t get_length() {
+		return metadata.length;
+	}
+	void set_value(std::string value) {
+		strcpy(data, value.c_str());
+	}
+	char* get_value() {
+		return data;
+	}
 };
--- Filelog-KV/gateway.cpp
+++ Filelog-KV/gateway.cpp
@@ -1,29 +1,81 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <tuple>
-#include <vector>
-#include <map>
+#include "rpc/server.h"
+#include "rpc/client.h"
+#include <thread> 
+#include <vector> 
+#include "data.hpp"
 
-#define BLOCK_SIZE 4096
-#define APPEND 1
-#define READ 0
+std::map<key_t,LBAs> K_LBAs;
 
-struct cmd {
-    int op; // append; read
-    int n_blks;
-    int blk_num;
-};
+void HandleRead() {
+    //TODO:
+    //read from local K-LBAs map
+    rpc::client rc("localhost", 5555);
+    rc.call("Read", ).as<void>();
+}
 
-struct reply {
-    int key_version;
-    off_t lba;
-};
 
-struct logent {
-    int key_ver;
-    int key;
-    char data[BLOCK_SIZE];
-};
+void HandleWrite(cmd command) {
+    //TODO:
+    //first connect to logger to submit the write request
+    //submit Append cmd to 3 loggers (currently only 3, fixed, no need to worry about hashing for now)
+    rpc::client wc("localhost", 6666);
 
-std::map<int, std::tuple<int, int, int>> key_lbas_map;
+    LogEnt logent;
+    logent.set_key(command.key);
+    logent.set_length(sizeof(command.value)); //TODO: check if this is the correct way to get the value's byte
+    logent.set_value(command.value);
+
+
+    off_t lba = wc.call("Append", logent).as<off_t>();
+    //then update local K-LBAs map
+    K_LBAs[command.key].lbas[0] = lba;
+}
+
+void HandleBroadcast(key_t key, off_t lba) {
+
+}
+
+int main() {
+    
+
+    std::thread read_thread([]() {
+        rpc::server rsrv(5555);
+        rsrv.bind("HandleRead", [](int a, int b) {
+            return a + b;
+        });
+        rsrv.run();
+    });
+
+    std::thread catchup_thread([]() {
+        rpc::server csrv(7777);
+        csrv.bind("CatchupMeeple", [](int a, int b) {
+            return a + b;
+        });
+        csrv.run();
+    });
+
+    constexpr int num_write_threads = 4; // Adjust this as needed
+    std::vector<std::thread> write_thread_pool;
+    for (int i = 0; i < num_write_threads; ++i) {
+        write_thread_pool.emplace_back([]() {
+            rpc::server wsrv(6666);
+            wsrv.bind("HandleWrite", [](int a, int b) {
+                return a + b;
+            });
+            wsrv.run();
+        });
+    }
+
+    read_thread.join();
+    for (auto& thread : write_thread_pool) {
+        thread.join();
+    }
+
+    return 0;
+}
+
+
+
