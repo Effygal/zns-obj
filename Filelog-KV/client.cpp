a5117cbe61993b496b5b8658ef85e17040e8d042
Dark <sinauthor2091@gmail.com>
2024-04-12 22:43:24 -0400

    two types for client code, type==0 for adding commands manually, type==1 for reading from a trace file given as command line argument

--- Filelog-KV/client.cpp
+++ Filelog-KV/client.cpp
@@ -6,7 +6,7 @@
     PUT <key> <value>
     GET <key>
 */
-
+#include <fstream>
 #include "client.hpp"
 #include "read_config.hpp"
 
@@ -55,54 +55,88 @@ bool parseCommand(const std::string& command, KVRequest& request) {
     return true;
 }
 
-int main() {
-    std::string command;
+void sendRequest(std::string command, Config& conf) {
     KVRequest request;
     std::string reply;
+
+    if (!parseCommand(command, request)) {
+        return;
+    }
+
+    int num_gateways = conf.gateways.size(); // how many gateways?
+    std::string gateway_ip;
+    int gateway_cport;
+
+    while (true) {
+        int rand = random() % num_gateways;
+        auto result = parseAddress(conf.gateways[rand]);
+        // Access elements of the returned tuple
+        gateway_ip = std::get<0>(result);
+        gateway_cport = std::get<2>(result);
+
+
+        try {
+            // Attempt to connect to the gateway
+            rpc::client client(gateway_ip, gateway_cport);
+            std::cout << "Connected to gateway " << gateway_cport << std::endl;
+            switch(request.request_type) {
+                case 2:
+                    reply = client.call("HandleWrite", request).as<std::string>();
+                    std::cout << reply << std::endl;
+                    break;
+                case 1:
+                    reply = client.call("HandleRead", request).as<std::string>();
+                    std::cout << "Read value: " << reply << std::endl;
+                    break;
+                default:
+                    break;
+            }
+            break;
+        } catch (const std::exception& e) {
+            // If connection fails, print error message and try the next gateway
+            std::cerr << "Error: " << e.what() << std::endl;
+            std::cout << "Failed to connect to gateway " << gateway_cport << ". Trying next gateway..." << std::endl;
+            continue;
+            }
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2 ) {
+        std::cerr << "Usage: ./client <type 0>" << std::endl;
+        return 1;
+    }
+    int request_type = atoi(argv[1]);
     // Read config.json file
     Config conf = parseConfig("config.json");
 
-    while (true) {
-        std::getline(std::cin, command);
+    std::string command;
 
-        if (!parseCommand(command, request)) {
-            continue;
-        }
-        int num_gateways = conf.gateways.size(); // how many gateways?
-        std::string gateway_ip;
-        int gateway_cport;
-
+    if (request_type == 0) {
         while (true) {
-            int rand = random() % num_gateways;
-            auto result = parseAddress(conf.gateways[rand]);
-            // Access elements of the returned tuple
-            gateway_ip = std::get<0>(result);
-            gateway_cport = std::get<2>(result);
-
-            try {
-                // Attempt to connect to the gateway
-                rpc::client client(gateway_ip, gateway_cport);
-                std::cout << "Connected to gateway " << gateway_cport << std::endl;
-                switch(request.request_type) {
-                    case 2:
-                        reply = client.call("HandleWrite", request).as<std::string>();
-                        std::cout << reply << std::endl;
-                        break;
-                    case 1:
-                        reply = client.call("HandleRead", request).as<std::string>();
-                        std::cout << "Read value: " << reply << std::endl;
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            } catch (const std::exception& e) {
-                // If connection fails, print error message and try the next gateway
-                std::cerr << "Error: " << e.what() << std::endl;
-                std::cout << "Failed to connect to gateway " << gateway_cport << ". Trying next gateway..." << std::endl;
-                continue;
-            }
+            std::getline(std::cin, command);
+            sendRequest(command, conf);
         }
+    }
+    else if (request_type == 1) {
+        std::string filePath = argv[2];
+
+        // Open the file
+        std::ifstream traceFile(filePath);
+        if (!traceFile.is_open()) {
+            std::cerr << "Failed to open file: " << filePath << std::endl;
+            return 1;
+        }
+
+        while (std::getline(traceFile, command)) {
+            sendRequest(command, conf);
+        }
+
+        // Close the file
+        traceFile.close();
+
+        
     } 
     return 0;
 }
